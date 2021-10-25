b27b284eaa6f747b826377be36dce863889577e3
vasanth0418 <itsvasanthboss@gmail.com>
2021-10-25 09:52:38 +0530

    surya

--- src/org/demo/Sample.java
+++ src/org/demo/Sample.java
@@ -2,6 +2,10 @@ package org.demo;
 
 public class Sample {
 
+	private static void empName() {
+		System.out.println("Name");
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
